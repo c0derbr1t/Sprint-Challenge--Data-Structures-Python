c5441a2b3a491b3b9acf815823c234da8061a6d5
c0derbr1t <c0der.br1t@gmail.com>
2020-09-11 10:16:47 -0500

    Create RingBuffer class with append and get methods.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.count = 0
+        self.storage = [None]*capacity
 
     def append(self, item):
-        pass
+        self.storage[self.count] = item
+        self.count += 1
+        
+        if self.count == self.capacity:
+            self.count = 0
 
     def get(self):
-        pass
+        return [item for item in self.storage if item is not None]
